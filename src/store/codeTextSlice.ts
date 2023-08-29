373413c45641e6f3769fa82b262e9190fc23a9f4
canaleal <alexcanales766@gmail.com>
2023-08-29 12:53:56 -0400

    Updated markdown viewer

--- src/store/codeTextSlice.ts
+++ src/store/codeTextSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const initialState = {
-    codeText: 'Test Test',
+    codeText: '',
 };
 
 export const codeTextSlice = createSlice({
